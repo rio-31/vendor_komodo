8c119532fb4f323a0b05df5920b3d51990d0b6f9
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-12-18 18:36:05 +0000

    vendor: Allow providing camera server and service
    Change-Id: I6d56930f4673cf7bdf99fccb87c15f0c0fc9a1d7
    Signed-off-by: Ahmad Thoriq Najahi <najahi@chips-projects.xyz>

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -4,4 +4,10 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 include vendor/komodo/config/BoardConfigQcom.mk
 endif
 
+ifneq ($(TARGET_USES_PREBUILT_CAMERA_SERVICE), true)
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/av/camera/cameraserver \
+    frameworks/av/services/camera/libcameraservice
+endif
+
 include vendor/komodo/config/BoardConfigSoong.mk
