DerpLab/platform_vendor_aosip

vendor: Add a bunch of accents for ThemePicker
Signed-off-by: spezi77 <spezi7713@gmx.net>
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

config/common.mk
@@ -116,6 +116,9 @@ include vendor/aosip/config/fonts.mk
 # Packages
 include vendor/aosip/config/packages.mk
 
+# Themes
+include vendor/aosip/config/accents.mk
+
 # Versioning
 include vendor/aosip/config/version.mk
 

rio-31/vendor_komodo

config/common.mk
@@ -124,6 +124,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/komodo/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/komodo/overlay/common
 
+# Themes
+include vendor/komodo/config/accents.mk
+
 # Some props that we need for the google stuff we're adding
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.ime.height_ratio=1.05 \