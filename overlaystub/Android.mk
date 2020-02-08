arghyac35/platform_vendor_aosp

vendor: initial overlay stub
Change-Id: I4e8325d8007a8349d8933e4b8e9a38c2a876cc17

overlaystub/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := AexOverlayStub
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)

rio-31/vendor_komodo

Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Signed-off-by: kry9ton <dhimasbagusprayoga@gmail.com>

overlaystub/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PACKAGE_NAME := komodoOverlayStub
+LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+include $(BUILD_PACKAGE)