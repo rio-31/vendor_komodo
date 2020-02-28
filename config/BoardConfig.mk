47bc2b3c914fb1a0cc884f90fc349f16d3db029d
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-02-28 14:17:36 -0300

    vendor: Add fwk-detect namespace
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I826946e5e7a3e951ceb699ed3b5791813be9ae5b

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -11,3 +11,8 @@ PRODUCT_SOONG_NAMESPACES += \
 endif
 
 include vendor/komodo/config/BoardConfigSoong.mk
+
+# Namespace for fwk-detect
+TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
+PRODUCT_SOONG_NAMESPACES += \
+    $(TARGET_FWK_DETECT_PATH)/fwk-detect
