631fafd7f171aec04234b3849b325fa24e5addb8
goodmeow <harunbam3@gmail.com>
2020-06-24 23:05:20 +0700

    config: common: Alphabetizing makefile purpose of this is to categorizing and making lines are easier to read, and some of lines are already moved out or removed due unused again. 1. File System tools was moved to packages https://github.com/Komodo-OS-Rom/vendor_komodo/commit/66a0091db368950f487ea7440ca513d09f32db78 to https://github.com/Komodo-OS-Rom/vendor_komodo/commit/353befad5842314c04cae1f20c162959cd4d9e35#diff-e6966a6927bf13b49c3daaf08679a5cb 2. removed persist.sys.dun.override is no longer needed.

--- config/common.mk
+++ config/common.mk
@@ -1,3 +1,122 @@
+# Copyright (C) 2020 Komodo-OS-Rom
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# ADB
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# Disable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
+else
+# Enable ADB authentication
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
+# ART do not include debug targets
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+# Strip the local variable table and the local variable type table to reduce
+# the size of the system image. This has no bearing on stack traces, but will
+# leave less information available via JDWP.
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/komodo/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/komodo/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    
+ifneq ($(AB_OTA_PARTITIONS),)
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
+    vendor/komodo/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
+    vendor/komodo/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
+endif
+
+# Branding
+include vendor/komodo/config/branding.mk
+
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+# Copy all custom init rc files
+$(foreach f,$(wildcard vendor/komodo/prebuilt/common/etc/init/*.rc),\
+    $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
+    
+# Copy over added mimetype supported in libcore.net.MimeUtils
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
+
+# Default notification/alarm sounds
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.config.notification_sound=Popcorn.ogg \
+    ro.config.alarm_alert=Bright_morning.ogg
+    
+# Dex Preopt Speed apps
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    SystemUI \
+    Settings \
+    KomodoQuickStep
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+    
+# G-Apps build type
+ifeq ($(CURRENT_BUILD_TYPE), gapps)
+include vendor/gapps/config.mk
+endif
+
+# Media
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    media.recorder.show_manufacturer_and_model=true
+    
+# OTA
+include vendor/komodo/config/ota.mk
+# ota allow downgrade
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.ota.allow_downgrade=true
+endif
+
+# Packages
+include vendor/komodo/config/packages.mk
+
+# Permissions 
+# permission Priv-App
+PRODUCT_COPY_FILES += \
+    vendor/komodo/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
+    vendor/komodo/config/permissions/privapp-permissions-fm.xml:system/etc/permissions/privapp-permissions-fm.xml \
+    vendor/komodo/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml \
+    vendor/komodo/config/permissions/privapp-permissions-system-komodo.xml:system/etc/permissions/privapp-permissions-system-komodo.xml \
+    vendor/komodo/config/permissions/privapp-permissions-product-komodo.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-komodo.xml
+
+# permission Google
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
+
+# permission OmniStyles
+PRODUCT_COPY_FILES += \
+    vendor/komodo/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml
+    
+# power whitelist
+PRODUCT_COPY_FILES += \
+    vendor/komodo/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
+    
+# Properties
 PRODUCT_BRAND ?= Komodo OS
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
@@ -22,122 +141,14 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.disable_rescue=true \
     ro.setupwizard.rotation_locked=true
 
-# Default notification/alarm sounds
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.config.notification_sound=Popcorn.ogg \
-    ro.config.alarm_alert=Bright_morning.ogg
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# Thank you, please drive thru!
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.dun.override=0
-endif
-
-ifeq ($(TARGET_BUILD_VARIANT),eng)
-# Disable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=0
-else
-# Enable ADB authentication
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
-endif
-
-# Backup Tool
-PRODUCT_COPY_FILES += \
-    vendor/komodo/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/komodo/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/komodo/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
-
-# OTA
-ifneq ($(TARGET_BUILD_VARIANT),user)
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.ota.allow_downgrade=true
-endif
-
-ifneq ($(AB_OTA_PARTITIONS),)
-PRODUCT_COPY_FILES += \
-    vendor/komodo/prebuilt/common/bin/backuptool_ab.sh:system/bin/backuptool_ab.sh \
-    vendor/komodo/prebuilt/common/bin/backuptool_ab.functions:system/bin/backuptool_ab.functions \
-    vendor/komodo/prebuilt/common/bin/backuptool_postinstall.sh:system/bin/backuptool_postinstall.sh
-endif
-
-# Some permissions
-PRODUCT_COPY_FILES += \
-    vendor/komodo/config/permissions/backup.xml:system/etc/sysconfig/backup.xml \
-    vendor/komodo/config/permissions/privapp-permissions-fm.xml:system/etc/permissions/privapp-permissions-fm.xml \
-    vendor/komodo/config/permissions/org.lineageos.snap.xml:system/etc/permissions/org.lineageos.snap.xml \
-    vendor/komodo/config/permissions/privapp-permissions-system-komodo.xml:system/etc/permissions/privapp-permissions-system-komodo.xml \
-    vendor/komodo/config/permissions/privapp-permissions-product-komodo.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-komodo.xml
-
-# Google permissions
-PRODUCT_COPY_FILES += \
-    vendor/komodo/prebuilt/common/etc/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
-
-# Copy all custom init rc files
-$(foreach f,$(wildcard vendor/komodo/prebuilt/common/etc/init/*.rc),\
-    $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))
-
-# Omnistyles permissions
-PRODUCT_COPY_FILES += \
-    vendor/komodo/prebuilt/etc/permissions/privapp-permissions-omni.xml:system/etc/permissions/privapp-permissions-omni.xml
-
-# Copy over added mimetype supported in libcore.net.MimeUtils
-PRODUCT_COPY_FILES += \
-    vendor/komodo/prebuilt/common/lib/content-types.properties:system/lib/content-types.properties
-
-# Enable SIP+VoIP on all targets
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
-
-# Enable wireless Xbox 360 controller support
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
-
-# Power whitelist
-PRODUCT_COPY_FILES += \
-    vendor/komodo/config/permissions/custom-power-whitelist.xml:system/etc/sysconfig/custom-power-whitelist.xml
-
-# Do not include art debug targets
-PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
-
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
-
-# Charger
-PRODUCT_PACKAGES += \
-    charger_res_images
-
-# Filesystems tools
-PRODUCT_PACKAGES += \
-    fsck.exfat \
-    fsck.ntfs \
-    mke2fs \
-    mkfs.exfat \
-    mkfs.ntfs \
-    mount.ntfs
-
-#Gvm
-include vendor/komodo/config/gvm.mk
-
-# Storage manager
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.storage_manager.enabled=true
-
-# Media
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    media.recorder.show_manufacturer_and_model=true
-
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/komodo/overlay
-DEVICE_PACKAGE_OVERLAYS += vendor/komodo/overlay/common
-
-# Themes
-include vendor/komodo/config/accents.mk
-include vendor/komodo/config/primary.mk
-
-# Some props that we need for the google stuff we're adding
+# properties of google IME
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.ime.height_ratio=1.05 \
     ro.com.google.ime.emoji_key=false
+    
+# Storage manager
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.storage_manager.enabled=true
 
 # Themed bootanimation
 TARGET_MISC_BLOCK_OFFSET ?= 0
@@ -146,27 +157,15 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     misc_writer_system \
     themed_bootanimation
-
-# Dex Preopt Speed apps
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    SystemUI \
-    Settings \
-    KomodoQuickStep
-
-# Branding
-include vendor/komodo/config/branding.mk
-
-# OTA
-include vendor/komodo/config/ota.mk
-
-# Packages
-include vendor/komodo/config/packages.mk
-
-ifeq ($(CURRENT_BUILD_TYPE), gapps)
-# GApps
-include vendor/gapps/config.mk
-endif
-
+    
+# Themes & Overlays
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/komodo/overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/komodo/overlay/common
+# overlays
+include vendor/komodo/config/accents.mk
+include vendor/komodo/config/primary.mk
+# GVisualMod
+include vendor/komodo/config/gvm.mk
 # Pixel Style
 include vendor/pixelstyle/config.mk
 
