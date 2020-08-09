Ancient-Lab/vendor_ancient

vendor: Add back livedisplay permissions
Signed-off-by: rezaadi0105 <rezaadipangestu5@gmail.com>

config/common.mk
@@ -58,6 +58,10 @@ PRODUCT_COPY_FILES += \
     vendor/ancient/config/permissions/privapp-permissions-product-google-ancient.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-google-ancient.xml \
     vendor/ancient/config/permissions/privapp-permissions-elgoog.xml:system/etc/permissions/privapp-permissions-elgoog.xml
 
+# Live Display
+PRODUCT_COPY_FILES += \
+    vendor/ancient/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml
+
 # Copy all ancient-OS-specific init rc files
 $(foreach f,$(wildcard vendor/ancient/prebuilt/common/etc/init/*.rc),\
     $(eval PRODUCT_COPY_FILES += $(f):system/etc/init/$(notdir $f)))

rio-31/vendor_komodo

Change-Id: Ifb1e42693694fbfdc55272c64540a6b7a92527a7

config/common.mk
@@ -169,7 +169,8 @@ PRODUCT_COPY_FILES += \
     vendor/komodo/config/permissions/privapp-permissions-fm.xml:system/etc/permissions/privapp-permissions-fm.xml \
     vendor/komodo/config/permissions/privapp-permissions-system-komodo.xml:system/etc/permissions/privapp-permissions-system-komodo.xml \
     vendor/komodo/config/permissions/privapp-permissions-product-komodo.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-product-komodo.xml \
-    vendor/komodo/config/permissions/privapp-permissions-recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-recorder.xml
+    vendor/komodo/config/permissions/privapp-permissions-recorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-recorder.xml \
+    vendor/komodo/config/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml
 
 # permission Google
 PRODUCT_COPY_FILES += \