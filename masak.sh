4f63e5a2428bc2985d0d52a7273b1998c46135e9
goodmeow <harunbam3@gmail.com>
2020-09-22 14:13:12 +0700

    masak: remove repo incase there is uncommited present
    Change-Id: I8b3fc9beeabd94d3c4d682a0524d530c7115b4d3

--- masak.sh
+++ masak.sh
@@ -330,6 +330,7 @@ fi
 if [ "$re_sync" = "yes" ]; then
     build_message "Sync repo"
     rm -rf external/motorola/faceunlock .repo/local_manifests
+    rm -rf device/xiaomi kernel/xiaomi vendor/xiaomi frameworks/base packagages/apps/Settings
     repo init -u https://github.com/Komodo-OS-Rom/manifest -b $BRANCH_MANIFEST
     repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
     git clone git@github.com:Komodo-OS-Rom/external_fu.git -b ten external/motorola/faceunlock
